90c7e37e435f9c47561bb232ed3af68ee796cd93
Mark Beeson <mark@beeson.email>
2022-04-01 21:49:32 -0700

    Documentation

--- src/handler.ts
+++ src/handler.ts
@@ -2,20 +2,27 @@ const cache: Cache = caches.default
 
 export async function handleRequest(request: Request): Promise<Response> {
   const idempotencyKey: string | null = request.headers.get('idempotency-key')
-  const response: Response = new Response(
-    `Hello, world ${new Date().toISOString()}`,
-  )
+  // Only run our code if the user supplied an Idempotency-Key header.
   if (idempotencyKey) {
     const host: string = request.headers.get('Host') || 'foo.com'
+    // Cloudflare requires the cache key be a well-formed url.
     const cacheKey = `https://${host}/__idempotency/${idempotencyKey}`
     const cachedResponse: Response | undefined = await cache.match(cacheKey)
     if (cachedResponse) {
+      // Cache hit.
+      // Just return our cached response.
       return cachedResponse
     } else {
+      // Cache miss.
+      // This is our mock response. You may want to run a fetch to your origin server.
+      const response: Response = new Response(
+        `Hello, world ${new Date().toISOString()}`,
+      )
+      // Save our response to the cache and return it.
       await cache.put(cacheKey, response.clone())
       return response
     }
   } else {
-    return response
+    return new Response(`You need an Idempotency-Key header.`)
   }
 }
